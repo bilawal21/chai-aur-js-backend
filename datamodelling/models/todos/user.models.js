d3aae6f69529218c7b882aeb0d6e89cfd447036c
EL PROFESOR <bilawal.ec21@gmail.com>
2025-02-02 15:53:51 +0500

    data modelling with mongoose

---
+++ datamodelling/models/todos/user.models.js
@@ -0,0 +1,26 @@
+import { Timestamp } from 'bson';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
