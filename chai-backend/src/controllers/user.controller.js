88e2d6a0c2649a5fe18c09099e4a6677e479a64e
bilawal21 <bilawal.ec21@gmail.com>
2025-02-16 21:22:41 +0500

    fix all register controller bugs

--- chai-backend/src/controllers/user.controller.js
+++ chai-backend/src/controllers/user.controller.js
@@ -7,7 +7,7 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
     const { fullName, email, username, password } = req.body;
-    console.log("email:", email);
+    // console.log("email:", email);
     
     // validation - not empty
     if (
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     // check if user already exists: username, email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -27,7 +27,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
